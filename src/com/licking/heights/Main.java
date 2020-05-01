cabd97987a7bce3edb6c3d4b1fff1cdf089e2d02
rebecca Powers <rebecca.powers.2021@student.lhschools.org>
2020-05-01 19:29:56 -0400

    Calculating days in Month

--- src/com/licking/heights/Main.java
+++ src/com/licking/heights/Main.java
@@ -29,5 +29,33 @@ public class Main {
         System.out.println("What year were you born?");
         year = keyboard.nextInt();
 
+        int monthAnswer = amountOfDaysInMonth(month, year);
+        System.out.println("There are " + monthAnswer + " days in this month");
     }
+        public static int amountOfDaysInMonth(int month, int year) {
+            switch (month) {
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                case 13:
+                    return 31;
+
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+
+                default:
+                    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+                        return 29;
+                    } else {
+                        return 28;
+                    }
+
+            }
+        }
 }
