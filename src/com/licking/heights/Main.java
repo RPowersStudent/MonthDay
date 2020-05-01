09501f49b861824d67309876c3c892f71d5ae766
rebecca Powers <rebecca.powers.2021@student.lhschools.org>
2020-05-01 10:56:24 -0400

    captured users input

--- src/com/licking/heights/Main.java
+++ src/com/licking/heights/Main.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int dayOfWeek;
+        int dayOfMonth;
+        int month;
+        int year;
+        int yearOfCentury;
+        int yearZeroBased;
+        Scanner Keyboard;
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("Monday's child is fair of face,");
         System.out.println("Tuesday's child is full of grace,");
         System.out.println("Wednesday's child is full of woe,");
@@ -14,6 +22,12 @@ public class Main {
         System.out.println("But the child born on the Sabbath Day,\n" +
                 "is fair and wise and good in every way.");
 
+        System.out.println("What month were you born? (Enter the number please)");
+        month = keyboard.nextInt();
+        System.out.println("What day were you born? (Enter the number please)");
+        dayOfMonth = keyboard.nextInt();
+        System.out.println("What year were you born?");
+        year = keyboard.nextInt();
 
     }
 }
