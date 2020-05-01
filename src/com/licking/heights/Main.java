020e332056c9a1fd4a6b160be759571ebf9faceb
rebecca Powers <rebecca.powers.2021@student.lhschools.org>
2020-05-01 10:54:03 -0400

    poem added

--- src/com/licking/heights/Main.java
+++ src/com/licking/heights/Main.java
@@ -1,8 +1,19 @@
 package com.licking.heights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Monday's child is fair of face,");
+        System.out.println("Tuesday's child is full of grace,");
+        System.out.println("Wednesday's child is full of woe,");
+        System.out.println("Thursday's child has far to go,");
+        System.out.println("Friday's child is loving and giving,");
+        System.out.println("Saturday's child works hard for a living,");
+        System.out.println("But the child born on the Sabbath Day,\n" +
+                "is fair and wise and good in every way.");
+
+
     }
 }
