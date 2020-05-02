1ed143b5b561af47cf1b8872936ed1f26ca93941
rebecca Powers <rebecca.powers.2021@student.lhschools.org>
2020-05-01 20:08:38 -0400

    zeller's congruence added

--- src/com/licking/heights/Main.java
+++ src/com/licking/heights/Main.java
@@ -32,30 +32,43 @@ public class Main {
         int monthAnswer = amountOfDaysInMonth(month, year);
         System.out.println("There are " + monthAnswer + " days in this month");
     }
-        public static int amountOfDaysInMonth(int month, int year) {
-            switch (month) {
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                case 13:
-                    return 31;
+    public static int amountOfDaysInMonth(int month, int year) {
+        switch (month) {
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+            case 13:
+                return 31;
 
-                case 4:
-                case 6:
-                case 9:
-                case 11:
-                    return 30;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
 
-                default:
-                    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-                        return 29;
-                    } else {
-                        return 28;
-                    }
-
-            }
+            default:
+                if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
         }
-}
+    }
+    public static int zellersCongruence(int month, int year, int dayOfMonth) {
+            if (month == 1 || month == 2) {
+                month = (month + 12);
+                year--;
+            }
+
+            int yearZero = (year / 100);
+            int zellerCongruenceEquation = 13 * (month + 1);
+            int yearCentury = (year % 100);
+            int dayOfWeek = ((dayOfMonth + (zellerCongruenceEquation/5)+ yearCentury +(yearCentury/4)+(yearZero/4)+(5*(yearZero)))%7);
+            return dayOfWeek;
+        }
+    }
+
+
